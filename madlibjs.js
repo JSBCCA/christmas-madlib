f7269db2a451b31dafe3d6ba7d59a1ad16755ab6
James Sibert <jsibert@basecampcodingacademy.org>
2016-12-07 15:01:00 -0600

    finished for now

--- madlibjs.js
+++ madlibjs.js
@@ -1,4 +1,19 @@
-// if eveything is filled out:
-// $('.white').css('visibility', 'visible');
-// otherwise:
-// $('.white').css('visibility', 'hidden');
+$("#button").on('click', function() {
+    for (i = 10; i > 0; i--) {
+      var x = $("#n"+i).val();
+      if (x.length !== 0) {
+        $('.n'+i).text(x);
+      }};
+    for (i = 3; i > 0; i--) {
+      var x = $("#a"+i).val();
+      if (x.length !== 0) {
+        $('.a'+i).text(x);
+      }};
+    for (i = 6; i > 0; i--) {
+      var x = $("#v"+i).val();
+      if (x.length !== 0) {
+        $('.v'+i).text(x);
+      }};
+    $('.white').css('visibility', 'visible');
+    return false;
+});
